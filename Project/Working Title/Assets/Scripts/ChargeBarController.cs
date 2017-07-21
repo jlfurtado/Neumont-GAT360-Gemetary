0dbe1704d20f59dedb4ae5ced4e04791df267281
Justin Furtado <justin.l.furtado@gmail.com>
2017-07-21 11:15:25 -0600

    misc enhancements

--- Project/Working Title/Assets/Scripts/ChargeBarController.cs
+++ Project/Working Title/Assets/Scripts/ChargeBarController.cs
@@ -8,16 +8,23 @@ public class ChargeBarController : MonoBehaviour {
     public Image frontImage;
     private PlayerController playerRef;
     private float heightStore;
+    private Color32 defaultColor;
+    private Color32 dodgeColor;
 
 	// Use this for initialization
 	void Start () {
         heightStore = frontImage.rectTransform.sizeDelta.y;
+        defaultColor = frontImage.color;
+        dodgeColor = new Color32(0, 255, 0, 255);
         playerRef = GameObject.FindGameObjectWithTag(Strings.PLAYER_TAG).GetComponent<PlayerController>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        float currentWidth = playerRef.GetEndurancePercent() * backImage.rectTransform.sizeDelta.x;
+        float perc = playerRef.GetEndurancePercent();
+        float currentWidth = perc * backImage.rectTransform.sizeDelta.x;
+        frontImage.color = perc < .5f ? defaultColor : dodgeColor;
+
         frontImage.rectTransform.sizeDelta = new Vector2(currentWidth, heightStore);
         //frontImage.enabled = currentWidth > 0.0f;
 	}
